15f5ce950116c5bc406729dbb8e5162c039ccb5c
Savel Suopea <ssuopea@c2r1p6.hive.fi>
2025-02-23 17:11:09 +0200

    added readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # piscine
+
 Tests for hive piscine
+
+
+# Evaluating
+
+1. `clone` the code to be evaluated, **giving it a name like c01**. (not C01) 
+2. check that there are no unnecessary files on the evaluated repository (test.sh will not test for extra files)
+3. clone the tests using `git clone https://github.com/suopea/piscine.git` (name  doesn't matter)
+4. `cd` to the test repository created on previous step
+5. test by running `sh test.sh PROJECT EXERCISE`, for example `sh test.sh 01 05` for project C01, exercise 05
+
--- test.sh
+++ test.sh
@@ -7,13 +7,13 @@ COLOR='\033[1;36m'
 WHITE='\033[0m'
 
 echo "\n  ${COLOR}   TEST FILE ${WHITE} \n"
-cat ./c0$1/ex0$2/main.c
+cat ./c$1/ex$2/main.c
 
 echo "\n  ${COLOR}   EXERCISE FILE ${WHITE}\n"
-cat ../c0$1/ex0$2/ft_*
+cat ../c$1/ex$2/ft_*
 
 echo "\n  ${COLOR}   NORMINETTE ${WHITE} \n"
-norminette -R CheckForbiddenSourceHeader ../c0$1/ex0$2/ft_*
+norminette -R CheckForbiddenSourceHeader ../c$1/ex$2/ft_*
 
-cc -Wall -Wextra -Werror ../c0$1/ex0$2/ft_* ./c0$1/ex0$2/main.c && echo "\n ${COLOR}    OUTPUT ${WHITE} \n" && ./a.out
+cc -Wall -Wextra -Werror ../c$1/ex$2/ft_* ./c$1/ex$2/main.c && echo "\n ${COLOR}    OUTPUT ${WHITE} \n" && ./a.out
 
