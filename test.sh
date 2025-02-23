0b3d24df1496e97d9773ede3f23b5a7e3a18909c
Savel Suopea <ssuopea@c2r1p6.hive.fi>
2025-02-23 14:48:24 +0200

    typo in cc arguments

--- test.sh
+++ test.sh
@@ -1,19 +1,21 @@
+#!/bin/sh
+
 ## first argument is PROJECT number without zero
 ## second argument is EXERCISE number without zero
 
 COLOR='\033[1;36m'
 WHITE='\033[0m'
 
-echo -e "\n  ${COLOR}  TEST FILE ${WHITE} \n"
+echo "\n  ${COLOR}  TEST FILE ${WHITE} \n"
 cat ./c0$1/ex0$2/main.c
 
-echo -e "\n\n"
-echo -e "  ${COLOR}  EXERCISE FILE ${WHITE}\n"
+echo "\n\n"
+echo "  ${COLOR}  EXERCISE FILE ${WHITE}\n"
 cat ../c0$1/ex0$2/ft_*
 
-echo -e "\n  ${COLOR}   NORMINETTE ${WHITE} \n"
+echo "\n  ${COLOR}   NORMINETTE ${WHITE} \n"
 norminette -R CheckForbiddenSourceHeader ../c0$1/ex0$2/ft_*
 
-echo -e "\n\n"
-cc -Wall -Wextra -Werror ../co$1/ex0$2/ft_* ./c0$1/ex0$2/main.c && echo -e "\n\n ${COLOR}   OUTPUT ${WHITE} \n" && ./a.out
+echo "\n\n"
+cc -Wall -Wextra -Werror ../c0$1/ex0$2/ft_* ./c0$1/ex0$2/main.c && echo -e "\n\n ${COLOR}   OUTPUT ${WHITE} \n" && ./a.out
 
