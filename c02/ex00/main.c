b948ccd29a05769996087299ef2d13e46edb47f0
Savel Suopea <ssuopea@c2r1p6.hive.fi>
2025-02-24 13:57:40 +0200

    clearer output for c02 ex00

--- c02/ex00/main.c
+++ c02/ex00/main.c
@@ -1,11 +1,15 @@
 #include <unistd.h>
+#include <stdio.h>
 
 char	*ft_strcpy(char *dest, char *src);
 
 int	main(void)
 {
 	char	original[] = "mango";
-	char	copy[5]; 
+	char	copy[] = "guava"; 
+
+	printf("Should be mango");
+	printf(":\n");
 	ft_strcpy(copy, original);
 	write(1, copy, sizeof(copy));
 }
