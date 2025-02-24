30635e6f21f927173316ff3049db9c34d724629b
Savel Suopea <ssuopea@c2r1p6.hive.fi>
2025-02-24 11:53:46 +0200

    added syntax highlighting

--- test.sh
+++ test.sh
@@ -3,18 +3,18 @@
 ## first argument is PROJECT number without zero
 ## second argument is EXERCISE number without zero
 
-COLOR='\033[1;36m'
+COLOR='\033[4;37m'
 WHITE='\033[0m'
 
-echo "\n  ${COLOR}   TEST FILE ${WHITE} \n"
-cat ./c$1/ex$2/main.c | sed 's/\t/    /g'
+echo "\n${COLOR}                                                                      TEST FILE${WHITE} \n"
+cat ./c$1/ex$2/main.c | pygmentize -l c -O style=monokai | sed 's/\t/    /g'
 
-echo "\n  ${COLOR}   EXERCISE FILE ${WHITE}\n"
-cat ../c$1/ex$2/ft_* | sed 's/\t/    /g'
+echo "\n${COLOR}                                                                  EXERCISE FILE${WHITE}\n"
+cat ../c$1/ex$2/ft_* | pygmentize -l c -O style=monokai | sed 's/\t/    /g'
 
-echo "\n  ${COLOR}   NORMINETTE ${WHITE} \n"
+echo "\n${COLOR}                                                                     NORMINETTE${WHITE} \n"
 norminette -R CheckForbiddenSourceHeader ../c$1/ex$2/ft_*
 
-echo "\n ${COLOR}    OUTPUT ${WHITE} \n" 
+echo "\n${COLOR}                                                                         OUTPUT${WHITE} \n" 
 cc -Wall -Wextra -Werror ../c$1/ex$2/ft_* ./c$1/ex$2/main.c && ./a.out
 
