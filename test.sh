1ee960d9b3241c4ebb494674a43d23d97a754791
Savel Suopea <ssuopea@c2r1p5.hive.fi>
2025-02-26 15:15:55 +0200

    added library detection

--- test.sh
+++ test.sh
@@ -3,7 +3,8 @@
 ## first argument is PROJECT number without zero
 ## second argument is EXERCISE number without zero
 
-COLOR='\033[4;37m'
+UNDERLINE='\033[4;37m'
+RED='\033[1;31m'
 WHITE='\033[0m'
 
 echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
@@ -12,16 +13,19 @@ cat ../s/c$1/ex$2/*.c | pygmentize -l c -O style=monokai | sed 's/\t/    /g'
 
 echo "\n\n\n\n\n\n\n\n\n"
 
-echo "\n\n\n${COLOR}                                                                      TEST FILE${WHITE} \n\n"
+echo "\n\n\n${UNDERLINE}                                                                      TEST FILE${WHITE} \n\n"
 cat ./c$1/ex$2/*.c | pygmentize -l c -O style=monokai | sed 's/\t/    /g'
 
-echo "\n\n\n${COLOR}                                                                  EXERCISE FILE${WHITE}\n\n"
+echo "\n\n\n${UNDERLINE}                                                                  EXERCISE FILE${WHITE}\n\n"
 cat ../c$1/ex$2/*.c | pygmentize -l c -O style=monokai | sed 's/\t/    /g'
 
-echo "\n\n\n${COLOR}                                                                     NORMINETTE${WHITE} \n\n"
+echo "\n\n\n${UNDERLINE}                                                                     NORMINETTE${WHITE} \n\n"
 norminette -R CheckForbiddenSourceHeader ../c$1/ex$2/*.c
 
-echo "\n\n\n${COLOR}                                                                         OUTPUT${WHITE} \n\n" 
+echo "\n\n\n${UNDERLINE}                                                                         OUTPUT${WHITE} \n\n" 
 cc -Wall -Wextra -Werror ../c$1/ex$2/*.c ./c$1/ex$2/*.c && ./a.out
 
-echo "\n\n${COLOR}                                                                    ${WHITE}  c$1 ex$2\n"
+echo "\n\n"
+cat ../c$1/ex$2/*.c | grep "#include" | sed 's/$/ LIBRARY DETECTED, is it allowed?/' | sed 's/#include//'
+
+echo "\n\n${UNDERLINE}                                                                    ${WHITE}  c$1 ex$2\n"
