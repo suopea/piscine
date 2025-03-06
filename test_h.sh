421c4f0ad2ed7cee4594dc8fd19fa54b51ba14e8
Savel Suopea <ssuopea@c2r1p12.hive.fi>
2025-03-06 11:28:34 +0200

    test_h.sh now temporarily moves the h file next to c file before compile

--- test_h.sh
+++ test_h.sh
@@ -24,13 +24,18 @@ cat ../c$1/ex$2/*.h | pygmentize -l c -O style=monokai | sed 's/\t/    /g'
 echo "\n\n\n${UNDERLINE}                                                                     NORMINETTE${WHITE} \n\n"
 norminette -R CheckDefine ../c$1/ex$2/*.h | sed 's/Error*/Error\o033[1;31m/' | sed 's/OK!/\o033[1;32m OK!/'
 
+cp ../c$1/ex$2/*.h ./c$1/ex$2/
+
 echo "\n\n\n${UNDERLINE}                                                                         OUTPUT${WHITE} \n\n" 
-cc -Wall -Wextra -Werror -fsanitize=address -fsanitize=leak -fsanitize=undefined -ggdb3 ../c$1/ex$2/*.h ./c$1/ex$2/*.c
+cc -Wall -Wextra -Werror -fsanitize=address -fsanitize=leak -fsanitize=undefined -ggdb3 ./c$1/ex$2/*.h ./c$1/ex$2/*.c
 
 echo "\n\n"
-cat ../c$1/ex$2/*.c | grep "#include" | sed 's/$/ \o033[1;31mEXTERNAL LIBRARY:\o033[0m is it allowed?/' | sed 's/#include/\n/'
 
-cc -g -Wall -Wextra -Werror ../c$1/ex$2/*.h ./c$1/ex$2/*.c && valgrind ./a.out
+cc -g -Wall -Wextra -Werror ./c$1/ex$2/*.h ./c$1/ex$2/*.c && valgrind ./a.out
+
+rm ./c$1/ex$2/*.h
+rm ./c$1/ex$2/*.gch
+rm ../c$1/ex$2/*.gch
 
 echo "\n\n${UNDERLINE}                                                                    ${WHITE}  c$1 ex$2\n"
 
