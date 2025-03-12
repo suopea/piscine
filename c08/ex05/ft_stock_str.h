8c87572ad8eda7eb960f998713df9284229b1258
Savel Suopea <ssuopea@c2r5p2.hive.fi>
2025-03-12 12:36:28 +0200

    c08

--- c08/ex05/ft_stock_str.h
+++ c08/ex05/ft_stock_str.h
@@ -6,14 +6,12 @@
 /*   By: ssuopea <ssuopea@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 15:55:43 by ssuopea           #+#    #+#             */
-/*   Updated: 2025/03/12 12:33:48 by ssuopea          ###   ########.fr       */
+/*   Updated: 2025/03/12 12:36:15 by ssuopea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_STOCK_STR_H 
 # define FT_STOCK_STR_H
-
-int	main(int argc, char **argv);
 
 typedef struct s_stock_str
 {
